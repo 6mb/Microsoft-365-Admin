b658a7fef2c4e564818ec0e4a5970820404bebad
huahui.wu <gayhub@live.cn>
2020-08-16 19:18:38 +0800

    功能完善

--- src/main/java/cn/itbat/microsoft/model/support/Sys.java
+++ src/main/java/cn/itbat/microsoft/model/support/Sys.java
@@ -2,11 +2,9 @@ package cn.itbat.microsoft.model.support;
 
 import cn.itbat.microsoft.utils.BetweenFormater;
 import cn.itbat.microsoft.utils.DateUtil;
+import lombok.Builder;
 import lombok.Data;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
 
 /**
@@ -19,7 +17,8 @@ public class Sys {
     /**
      * 项目路径
      */
-    private String projectDir;
+    @Builder.Default
+    private String projectDir = "";
 
     /**
      * 操作系统
@@ -45,13 +44,6 @@ public class Sys {
         this.osName = System.getProperty("os.name");
         this.osArch = System.getProperty("os.arch");
         this.osVersion = System.getProperty("os.version");
-        Resource resource = new ClassPathResource("");
-        try {
-            this.projectDir = resource.getFile().getAbsolutePath();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         long uptime = ManagementFactory.getRuntimeMXBean().getUptime();
         this.upTime = DateUtil.formatBetween(uptime, BetweenFormater.Level.SECOND);
     }
